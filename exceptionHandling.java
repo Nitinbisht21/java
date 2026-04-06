aba4b8534a3f0c7d669af16f794f1a906329ffaf
Nitinbisht21 <nitinbisht652005@gmail.com>
2026-04-06 20:24:14 +0530

    my project files

---
+++ exceptionHandling.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class tc{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter two numbwer");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        try{
+            int result = a/b;
+            System.out.println("Result ="+result);
+        }
+        catch(ArithmeticException e){
+            System.out.println("can not divided by zero");
+        }
+    }
+}
